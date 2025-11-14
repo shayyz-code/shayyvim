14e2c8e7502de2d35fa22044ed29628e7525862b
shayyz-code <aungminkhant.shay@gmail.com>
2025-11-14 19:58:28 +0630

    Add keymaps for moving lines and buffer navigation
    Introduces keybindings to move lines up and down in normal, visual, and insert modes using Alt+j/k. Also adds Alt+[ and Alt+] shortcuts for navigating to previous and next buffers, respectively.

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,19 @@ vim.cmd "set softtabstop=2"
 vim.cmd "set shiftwidth=2"
 vim.cmd "set number"
 
+
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==") -- move line down(n)
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==") -- move line up(n)
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line down(v)
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line up(v)
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true }) -- move line down(i)
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true }) -- move line up(i)
+
+-- Move to previous buffer
+vim.keymap.set("n", "<A-[>", ":BufferPrevious<CR>", { silent = true })
+
+-- Move to next buffer
+vim.keymap.set("n", "<A-]>",     ":BufferNext<CR>", { silent = true })
 
