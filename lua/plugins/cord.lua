9f7348bc2b4a9208833f47017432b9f4bc0acd6c
shayyz-code <aungminkhant.shay@gmail.com>
2025-11-14 16:25:26 +0630

    Update file permissions and refactor cord plugin config
    Changed file permissions from 644 to 755 for multiple project files. Refactored the cord.nvim plugin configuration to use the 'opts' pattern and simplified setup.

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,20 +1,11 @@
 return {
   "vyfor/cord.nvim",
+  name = "cord",
+  lazy = false,
   build = ":Cord update",
-  config = function()
-    require("cord").setup {
-      editor = {
-        client = "neovim",
-        tooltip = "Syrh Syr G dey thone tk Text Editor",
-        icon = nil,
-      },
-      display = {
-        theme = "catppuccin",
-        flavor = "dark",
-        view = "full",
-        swap_fields = false,
-        swap_icons = false,
-      },
-    }
+  opts = {},
+  config = function(_, opts)
+    require('cord').setup(opts)
   end,
 }
+
